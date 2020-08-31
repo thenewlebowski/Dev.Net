c90d48cd402296cf00393dcc013f95632e410751
Colton Nielsen <colton.finch@gmail.com>
2020-08-31 20:05:24 +0000

    Profile description is now its own functional component

---
+++ client/src/Components/profile/ProfileDesc/ProfileDesc.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './ProfileDesc.module.css';
+
+export default function ProfileDesc(props) {
+    return (
+        <div className={classes.profileDesc}>
+            <div className={classes.descHeader}>
+                <h1 className={classes.username}>
+                        {props.username}
+                </h1>
+                <button onClick={props.editToggle}>
+                    {props.editMode ? "Cancel" : "Edit"}
+                </button>
+            </div>
+            
+            <div className={ classes.infoContainer }>
+                <div className={ classes.bioContainer }>
+                    <h5>Bio</h5> 
+                    <p>{props.bio} </p>
+                </div>
+                <div className={ classes.langsContainer }>
+                    <h5>Fluent languages:</h5>
+                    <ul>
+                        {props.languages(props.langs)}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
