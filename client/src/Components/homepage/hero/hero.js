53b92a8c6d9e22a377f7996587310fe42cbc9995
Coltonn <Cltnnielsen@gmail.com>
2020-10-26 23:15:20 +0000

    Moved styling on Hero

--- client/src/Components/homepage/hero/hero.js
+++ client/src/Components/homepage/hero/hero.js
@@ -5,7 +5,7 @@ import heroPic from './hero_pic.jpg';
 export default function Hero() {
     return (
         <div className={classes.hero} style={{overflow: 'hidden'}}>
-            <img className={classes.heroImg} src={heroPic} style={{width: '100%', top: '-75%', position: 'relative'}}/>
+            <img className={classes.heroImg} src={heroPic}/>
         </div>
     )
 }
