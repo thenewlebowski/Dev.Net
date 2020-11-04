31ae8d5167e7a02ffd75d44b2e2cd318b7a0039b
Coltonn <Cltnnielsen@gmail.com>
2020-11-04 23:08:29 +0000

    Made Desc to be more dynamic so we can reuse the same component

--- client/src/Components/homepage/desc/Desc.js
+++ client/src/Components/homepage/desc/Desc.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './Desc.module.css';
+import { Link } from 'react-router-dom';
 
-export default function Desc() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Desc(props) {
+    
+    useEffect(() => {
+
+    }, [])
+
     return (
-        <div className={classes.descContainer}>
-            <div className={classes.descInnerContainer}>
-                <h1 className={classes.descHeader}>Connect. Build. Succeed.</h1>
-                <p className={classes.descDesc}>Software development careers are estimated to grow 50 percent by the year 2025. Our goal is to educate future developers of up and coming technologies. In our efforts we hope to establish an ecosystem of thriving developers that continue to grow and keep our ideologies strong by educating their own peers. If you are interested in joining the Dev Army click the link below. </p>
-                <span><button className={"btn btnPrimary " + classes.descBtn}>Join Now</button></span>
+        <div className={classes.descContainer} style={{ zIndex: props.zIndex }}>
+            <div className={classes.descInnerContainer}  style={{ textAlign: props.textAlign }} >
+                <h1 className={classes.descHeader}>{props.header}  <FontAwesomeIcon icon={props.iconHeader}/></h1>
+                <p className={classes.descDesc}>{props.body}</p>
+                
+                { props.href ? 
+                    <span>
+                        <Link to={props.href} className={"btn btnPrimary " + classes.descBtn}>
+                            {props.btnText}  <FontAwesomeIcon icon={props.iconBtn} />
+                        </Link>
+                    </span> 
+                : null }
+
             </div>
         </div>
     )
