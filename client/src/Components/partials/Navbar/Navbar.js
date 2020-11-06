298402c3d193998b339a945968a4497865b29069
Coltonn <Cltnnielsen@gmail.com>
2020-11-06 16:50:26 +0000

    Styling changes to navbar

--- client/src/Components/partials/Navbar/Navbar.js
+++ client/src/Components/partials/Navbar/Navbar.js
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../../actions/authActions';
 import classes from './Navbar.module.css';
 
+//======FONT AWESOME ICONS=======//
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+
 export default function Navbar() {
     const auth = useSelector(state => state.auth);
     const dispatch = useDispatch();
@@ -16,9 +20,10 @@ export default function Navbar() {
                         <Link to="/" >Dev.Net</Link>
                     </div>
                     <div className={classes.navItems}>
+                        {!auth.isAuthenticated ? <Link to="/login" >Login</Link> : <Link to="/profile" >Profile</Link>}
+                        {!auth.isAuthenticated ? <Link to="/signup" >Sign Up</Link> : <Link to="/wall">Wall</Link>}
                         {auth.isAuthenticated ? <Link to="#" onClick={()=>dispatch(logoutUser())}>Logout</Link> : null}
-                        {!auth.isAuthenticated ? <Link to="/login" >Login</Link> : null}
-                        {!auth.isAuthenticated ? <Link to="/signup" >Sign Up</Link> : null}
+                        {auth.isAuthenticated ? <Link to="/settings"> <FontAwesomeIcon icon={faCog} /> </Link> : null}
                     </div>
                 </div>
             </div>
