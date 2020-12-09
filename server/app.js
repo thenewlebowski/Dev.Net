000f5018eae90e01031743518a4cc1300dcbd364
Coltonn <Cltnnielsen@gmail.com>
2020-12-09 01:10:44 +0000

    [DOTENV] Debug feature on backend

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/uploads', express.static('uploads'));
-require('dotenv').config();
+require('dotenv').config({ debug: process.env.DOTENV_DEBUG });
 
 app.use(cors());
 app.use(express.json());
