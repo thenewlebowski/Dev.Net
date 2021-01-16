b66a6992b764df37088e3a75e26235fc58f3574c
Coltonn <Cltnnielsen@gmail.com>
2021-01-16 05:31:17 +0000

    [EditorJs] Added editor js for blog body

--- admin/src/views/blog/Form.js
+++ admin/src/views/blog/Form.js
@@ -16,6 +16,9 @@ import {
   } from '@coreui/react'
   import CIcon from '@coreui/icons-react'
 
+import EditorJs from 'react-editor-js';
+import { EDITOR_JS_TOOLS } from '../../tool';
+
 export default function Form() {
     return (
         <CRow>
@@ -50,233 +53,29 @@ export default function Form() {
                 </CFormGroup>
               
                 <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="textarea-input">Body</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CTextarea 
-                      name="textarea-input" 
-                      id="textarea-input" 
-                      rows="9"
-                      placeholder="Content..." 
-                    />
-                  </CCol>
+                    <CCol md="3">
+                      <CLabel htmlFor="textarea-input">Body</CLabel>
+                    </CCol>
+                    <CCol md="9">
+                      <div className="editor-js-container">
+                        <EditorJs
+                        style={{border: '1px solid black'}}
+                        tools={ EDITOR_JS_TOOLS }
+                        />
+                      </div>
+                    </CCol>
                 </CFormGroup>
 
                 <CFormGroup row>
                   <CCol md="3">
                     <CLabel htmlFor="tag-input">Tags</CLabel>
                   </CCol>
-
                   <CCol xs="12" md="9">
                     <CInput id="tag-input" name="tag-input" placeholder="Tags" />
                     <CFormText>What kind of tags should go along with this post</CFormText>
                   </CCol>
                 </CFormGroup>
 
-                {/* <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="select">Select</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CSelect custom name="select" id="select">
-                      <option value="0">Please select</option>
-                      <option value="1">Option #1</option>
-                      <option value="2">Option #2</option>
-                      <option value="3">Option #3</option>
-                    </CSelect>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="selectLg">Select Large</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9" size="lg">
-                    <CSelect custom size="lg" name="selectLg" id="selectLg">
-                      <option value="0">Please select</option>
-                      <option value="1">Option #1</option>
-                      <option value="2">Option #2</option>
-                      <option value="3">Option #3</option>
-                    </CSelect>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="selectSm">Select Small</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CSelect custom size="sm" name="selectSm" id="SelectLm">
-                      <option value="0">Please select</option>
-                      <option value="1">Option #1</option>
-                      <option value="2">Option #2</option>
-                      <option value="3">Option #3</option>
-                      <option value="4">Option #4</option>
-                      <option value="5">Option #5</option>
-                    </CSelect>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="disabledSelect">Disabled Select</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CSelect 
-                      custom 
-                      name="disabledSelect" 
-                      id="disabledSelect" 
-                      disabled 
-                      autoComplete="name"
-                    >
-                      <option value="0">Please select</option>
-                      <option value="1">Option #1</option>
-                      <option value="2">Option #2</option>
-                      <option value="3">Option #3</option>
-                    </CSelect>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol tag="label" sm="3" className="col-form-label">
-                    Switch checkboxes
-                  </CCol>
-                  <CCol sm="9">
-                    <CSwitch
-                      className="mr-1"
-                      color="primary"
-                      defaultChecked
-                    />
-                    <CSwitch
-                      className="mr-1"
-                      color="success"
-                      defaultChecked
-                      variant="outline"
-                    />
-                    <CSwitch
-                      className="mr-1"
-                      color="warning"
-                      defaultChecked
-                      variant="opposite"
-                    />
-                    <CSwitch
-                      className="mr-1"
-                      color="danger"
-                      defaultChecked
-                      shape="pill"
-                    />
-                    <CSwitch
-                      className="mr-1"
-                      color="info"
-                      defaultChecked
-                      shape="pill"
-                      variant="outline"
-                    />
-                    <CSwitch
-                      className="mr-1"
-                      color="dark"
-                      defaultChecked
-                      shape="pill"
-                      variant="opposite"
-                    />
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel>Radios</CLabel>
-                  </CCol>
-                  <CCol md="9">
-                    <CFormGroup variant="checkbox">
-                      <CInputRadio className="form-check-input" id="radio1" name="radios" value="option1" />
-                      <CLabel variant="checkbox" htmlFor="radio1">Option 1</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="checkbox">
-                      <CInputRadio className="form-check-input" id="radio2" name="radios" value="option2" />
-                      <CLabel variant="checkbox" htmlFor="radio2">Option 2</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="checkbox">
-                      <CInputRadio className="form-check-input" id="radio3" name="radios" value="option3" />
-                      <CLabel variant="checkbox" htmlFor="radio3">Option 3</CLabel>
-                    </CFormGroup>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel>Inline Radios</CLabel>
-                  </CCol>
-                  <CCol md="9">
-                    <CFormGroup variant="custom-radio" inline>
-                      <CInputRadio custom id="inline-radio1" name="inline-radios" value="option1" />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-radio1">One</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="custom-radio" inline>
-                      <CInputRadio custom id="inline-radio2" name="inline-radios" value="option2" />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-radio2">Two</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="custom-radio" inline>
-                      <CInputRadio custom id="inline-radio3" name="inline-radios" value="option3" />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-radio3">Three</CLabel>
-                    </CFormGroup>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3"><CLabel>Checkboxes</CLabel></CCol>
-                  <CCol md="9">
-                    <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox 
-                        id="checkbox1" 
-                        name="checkbox1" 
-                        value="option1" 
-                      />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox1">Option 1</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox id="checkbox2" name="checkbox2" value="option2" />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox2">Option 2</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox id="checkbox3" name="checkbox3" value="option3" />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox3">Option 3</CLabel>
-                    </CFormGroup>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel>Inline Checkboxes</CLabel>
-                  </CCol>
-                  <CCol md="9">
-                    <CFormGroup variant="custom-checkbox" inline>
-                      <CInputCheckbox 
-                        custom 
-                        id="inline-checkbox1" 
-                        name="inline-checkbox1" 
-                        value="option1" 
-                      />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-checkbox1">One</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="custom-checkbox" inline>
-                      <CInputCheckbox custom id="inline-checkbox2" name="inline-checkbox2" value="option2" />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-checkbox2">Two</CLabel>
-                    </CFormGroup>
-                    <CFormGroup variant="custom-checkbox" inline>
-                      <CInputCheckbox custom id="inline-checkbox3" name="inline-checkbox3" value="option3" />
-                      <CLabel variant="custom-checkbox" htmlFor="inline-checkbox3">Three</CLabel>
-                    </CFormGroup>
-                  </CCol>
-                </CFormGroup> */}
-                {/* <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel>Multiple File input</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CInputFile 
-                      id="file-multiple-input" 
-                      name="file-multiple-input" 
-                      multiple
-                      custom
-                    />
-                    <CLabel htmlFor="file-multiple-input" variant="custom-file">
-                      Choose Files...
-                    </CLabel>
-                  </CCol>
-                </CFormGroup> */}
               </CForm>
               <CFormGroup row>
                 <CCol md="3">
