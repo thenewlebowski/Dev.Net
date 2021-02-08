d56ef727b4073cc925cd721b1a716d758594e0ba
Coltonn <Cltnnielsen@gmail.com>
2021-02-09 03:22:13 +0500

    [Post] Commands that correspond to moving post from admin to node db

--- admin-server/app.js
+++ admin-server/app.js
@@ -2,6 +2,7 @@ let cookieParser = require('cookie-parser');
 let createError = require('http-errors');
 let mongoose = require('mongoose');
 let express = require('express');
+let logger = require('morgan');
 let cors = require('cors');
 let path = require('path');
 require('dotenv').config();
@@ -13,13 +14,22 @@ const seed = require('./seeds/seeds.js');
 let app = express();
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'models')));
 
+
+//mongoose options
+const mongooseOpt = {
+  useNewUrlParser     : true,
+  useUnifiedTopology  : true,
+  useFindAndModify    : false,
+} 
 //Connect to DB
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.ADMIN_MONGODB_URI, mongooseOpt)
 mongoose.connection.once('open', ()=>{
     console.log('MongoDB database connection established successfully')
 });
@@ -37,7 +47,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in developmentp
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
